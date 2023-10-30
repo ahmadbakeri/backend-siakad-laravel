8268dd3a0b9585cd6610d83bc0ccbe7caac4f215
ahmadbakeri <ahmadbakeri1621@gmail.com>
2023-10-31 00:55:13 +0800

    crud users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,13 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        User::factory(200)->create();
 
         User::create([
             'name' => 'Ahmad Bakeri',
             'email' => 'ahmadbakeri@fic8.com',
             'email_verified_at' => now(),
-            'password' => Hash::make('123456543'),
+            'password' => Hash::make('12345678'),
+            'roles' => 'admin',
         ]);
     }
 }
